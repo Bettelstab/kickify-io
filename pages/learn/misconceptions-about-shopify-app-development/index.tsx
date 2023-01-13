74f22d20dd80907cf4c54b5cf1630bd1c89b25ee
Maximilian Harz <maxharz@gmail.com>
2023-01-13 15:37:34 +0100

    Add common misconceptions to knowledge base

---
+++ pages/learn/misconceptions-about-shopify-app-development/index.tsx
@@ -0,0 +1,40 @@
+import BasicPage from "/components/basic-page/BasicPage";
+
+export default function MisconceptionsAboutShopifyAppDevelopment() {
+  return (
+    <BasicPage
+      title="Misconceptions about Shopify App Development"
+      description="What are the most common misconceptions about Shopify App Development?"
+    >
+      <p>Misconceptions about Shopify App Development</p>
+
+      <ul>
+        <li>
+          <strong>It's Too Difficult:</strong> Many people think that developing
+          an app for Shopify is overly complicated and time-consuming. However,
+          the Shopify App Store makes it easy to create an app for Shopify and
+          the platform provides helpful resources to guide the development
+          process.
+        </li>
+        <li>
+          <strong>You Need To Be A Coder:</strong> While coding is required to
+          develop a Shopify app, it is not necessary to be a professional coder
+          to create a basic app. With the help of tutorials and documentation,
+          anyone can develop an app for Shopify.
+        </li>
+        <li>
+          <strong>It's Too Expensive:</strong> Developing an app for Shopify can
+          be done on a budget. There are many free resources available to help
+          developers build their apps, as well as many free app templates that
+          can be used to quickly create an app.
+        </li>
+        <li>
+          <strong>It Requires Constant Maintenance:</strong> Maintaining an app
+          for Shopify does not require too much work. The platform makes it easy
+          to keep the app up-to-date, and the app store also provides helpful
+          resources to help with any maintenance needs.
+        </li>
+      </ul>
+    </BasicPage>
+  );
+}
