e3e7e6428b92e304ec48d5aeee6bc1c600d2df2a
Maximilian Harz <maxharz@gmail.com>
2023-01-18 00:09:02 +0100

    Center hero, button placement, finetuning

--- pages/home/Hero.tsx
+++ pages/home/Hero.tsx
@@ -6,28 +6,28 @@ import heroImage from "/public/images/angry-dev-3000.jpg";
 export default function Hero() {
   return (
     <div className={styles.hero}>
-      <div className={styles.grid}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>Deploy your Shopify App in minutes</h1>
-          <p className={styles.description}>
-            Developing and deploying a Shopify App can be cumbersome. Take a
-            shortcut and do it the <em>Shopkick</em> way.
-          </p>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Deploy your Shopify App in minutes</h1>
+        <p className={styles.description}>
+          Developing and deploying a Shopify App can be cumbersome. Take a
+          shortcut and do it the <em>Shopkick</em> way.
+        </p>
+        <div className={styles.cta}>
           <Link href="/signup">
             <button>Get started</button>
           </Link>
         </div>
-        <div className={styles.container}>
-          <div className={styles.imageContainer}>
-            <Image
-              className={styles.heroImage}
-              src={heroImage}
-              placeholder="blur"
-              alt="Angry developer wants to deploy his Shopify App"
-              priority
-              fill
-            />
-          </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.heroImage}
+            src={heroImage}
+            placeholder="blur"
+            alt="Angry developer wants to deploy his Shopify App"
+            priority
+            fill
+          />
         </div>
       </div>
     </div>
