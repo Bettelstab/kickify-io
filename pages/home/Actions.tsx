d3c5acc40df4338922a1f589f01a2fd7e1e2f373
Maximilian Harz <maxharz@gmail.com>
2023-01-17 22:52:49 +0100

    Overhaul contact form and actions grid

---
+++ pages/home/Actions.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "./Actions.module.css";
+
+export default function Actions() {
+  return (
+    <div className={styles.grid}>
+      {/*
+  <a href="/docs" className={styles.card}>
+    <h2>Documentation &rarr;</h2>
+    <p>Find in-depth information about Shopkick.io features and API.</p>
+  </a>
+  */}
+
+      <Link href="/learn" className={styles.card}>
+        <h2>Learn &rarr;</h2>
+        <p>Learn about the possibilities of Shopify apps</p>
+      </Link>
+
+      <Link href="/get-started" className={styles.card}>
+        <h2>Get started &rarr;</h2>
+        <p>Examples to get you started quickly</p>
+      </Link>
+
+      <Link href="/deploy" className={styles.card}>
+        <h2>Deploy &rarr;</h2>
+        <p>
+          Instantly deploy our Shopify App template or your own code to
+          Shopkick.io
+        </p>
+      </Link>
+    </div>
+  );
+}
--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -1,11 +1,11 @@
 import BasicPage from "/components/basic-page";
+import styles from "./Home.module.css";
 import Features from "./Features";
 import Hero from "./Hero";
 import PreBuilt from "./PreBuilt";
 import Pricing from "./Pricing";
-import styles from "./Home.module.css";
-import Link from "next/link";
 import Contact from "./Contact";
+import Actions from "./Actions";
 
 export default function Home() {
   return (
@@ -15,37 +15,11 @@ export default function Home() {
       <PreBuilt />
       <Pricing />
 
-      <Contact />
-
       <div className={styles.grid}>
-        {/*
-        <a href="/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Shopkick.io features and API.</p>
-        </a>
-        */}
-
-        <Link href="/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about the possibilities of Shopify apps</p>
-        </Link>
-
-        <Link href="/get-started" className={styles.card}>
-          <h2>Get started</h2>
-          <p>Examples to get you started quickly</p>
-        </Link>
-
-        <Link
-          href="/deploy"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy our Shopify App template or your own code to
-            Shopkick.io
-          </p>
-        </Link>
+        <Contact />
+        <Actions />
       </div>
+
     </BasicPage>
   );
 }
