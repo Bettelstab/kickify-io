98bbe9b98158390a55d90fc83838719cb1258501
Maximilian Harz <maxharz@gmail.com>
2023-01-18 17:19:49 +0100

    Add rocket emoji to hero

--- pages/home/Hero.tsx
+++ pages/home/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <div className={styles.hero}>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Deploy your Shopify App in minutes</h1>
+        <h1 className={styles.title}>🚀 Deploy your Shopify App in minutes</h1>
         <p className={styles.description}>
           Developing and deploying a Shopify App can be cumbersome. Take a
           shortcut and do it the <em>Kickify</em> way.
