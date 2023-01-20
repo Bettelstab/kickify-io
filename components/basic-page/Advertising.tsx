f65b85620deaa49380b6c0ae4a99770f9d4ceddc
Maximilian Harz <maxharz@gmail.com>
2023-01-21 00:22:31 +0100

    Overhaul advertising

--- components/basic-page/Advertising.tsx
+++ components/basic-page/Advertising.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import styles from "./Advertising.module.css";
 
 export default function Advertising() {
   return (
-    <div>
-      Are you looking for an easy way to deploy your Shopify App, or need some
-      help to get started? Have a look at what Kickify.io can offer you.
-      <Link href="/"><button>Read more</button></Link>
+    <div className={styles.advertising}>
+      <span className={styles.icon}>🚀</span>
+      Are you looking for an easy way to deploy your Shopify App as fast as possible? Or do you need some
+      help to get started? Have a look at what Kickify.io has to offer.
+      <Link href="/">
+        <button>Read more</button>
+      </Link>
     </div>
   );
 }
