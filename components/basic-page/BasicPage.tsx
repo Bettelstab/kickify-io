52e8d2ce26ba336db78c8fb3b402ac0cd8bbeba0
Maximilian Harz <maxharz@gmail.com>
2023-01-20 14:10:17 +0100

    Add new favicon

--- components/basic-page/BasicPage.tsx
+++ components/basic-page/BasicPage.tsx
@@ -28,10 +28,6 @@ export default function BasicPage({
       <Head>
         <title>{pageTitle}</title>
         <meta name="description" content={description} />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
-        />
       </Head>
 
       <NavigationBar />
